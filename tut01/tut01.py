4d01c1caf5813c2222d14995b7bf8c05568989f1
Abhinav Mishra <abhinav_2001mm01@iitp.ac.in>
2022-09-09 21:19:22 +0530

    Copied Tutorial
    Copied tut01 from CS384_2022 Repository

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,10 +1,6 @@
-def factorial (x):
-    fact=1
-    for i in range(1,x+1):
-        fact=fact*i
-    print("The factorial of",x, "is",fact)
+def octact_identification(mod=5000):
+###Code
 
-    
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+mod=5000
+octact_identification(mod)
