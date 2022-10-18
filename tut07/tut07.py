e5fe7fd0db3128fdaa1a46f0572973616625c895
Abhinav Mishra <abhinav_2001mm01@iitp.ac.in>
2022-10-19 05:29:54 +0530

    Transition Count Code Done

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -5,6 +5,7 @@ from platform import python_version
 import os
 import openpyxl_dictreader
 from openpyxl import Workbook
+import math
 os.system("cls")
 start_time = datetime.now()
 
@@ -48,7 +49,7 @@ def octant_rank_count(count):
 # Function - 2 to write the octant range names
 
 
-def octant_range_names(inputfile, mod=5000):
+def octant_range_names(sheet, inputfile, mod=5000):
     octant_name_id_mapping = {"1": "Internal outward interaction", "-1": "External outward interaction", "2": "External Ejection",
                               "-2": "Internal Ejection", "3": "External inward interaction", "-3": "Internal inward interaction", "4": "Internal sweep", "-4": "External sweep"}
     # Declaring the lists to store the values
@@ -262,10 +263,6 @@ def octant_range_names(inputfile, mod=5000):
         first_rank_name.append("")
 
     try:
-        # Output the file to output_octant_transition_identify.xlsx
-        wb = Workbook()
-        sheet = wb.active
-
         # Header line1
         header_line1 = ["", "", "", "", "", "", "", "", "",
                         "", "", " ", " ", "Overall Octant Count", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
@@ -363,15 +360,297 @@ def octant_range_names(inputfile, mod=5000):
         print("Something went wrong while writing to octant_output.csv")
         exit()
 
-    finally:
-        # Saving the workbook.
-        wb.save("octant_output_ranking_excel.xlsx")
-        wb.close()  # Closing the workbook.
+
+# Function - 3 to write the transition count
+
+def octant_transition_count(sheet, inputfile, mod=5000):
+
+    # Declaring the lists to store the values
+    time = []
+    u = []
+    v = []
+    w = []
+    u_prime = []
+    v_prime = []
+    w_prime = []
+    octant = []
+
+    # Opening the input_octant_transition_identify.xlsx file in read mode
+    try:
+        reader = openpyxl_dictreader.DictReader(
+            inputfile, "Sheet1")
+
+        # Storing the values of each key in the corresponding lists
+        for row in reader:
+            time.append(float(row['T']))
+            u.append(float(row['U']))
+            v.append(float(row['V']))
+            w.append(float(row['W']))
+
+        # Calculating the average of U, V, W
+        u_avg = sum(u)/len(u)
+        v_avg = sum(v)/len(v)
+        w_avg = sum(w)/len(w)
+
+        # Data Preprocessing - Calculating the difference between the velocities and their respective average values and storing in the respective lists.
+        for u_value in u:
+            u_prime.append(u_value-u_avg)
+        for v_value in v:
+            v_prime.append(v_value-v_avg)
+        for w_value in w:
+            w_prime.append(w_value-w_avg)
+    # FileNotFound Error
+    except FileNotFoundError:
+        print("Input File not found!")
+        exit()
+    # Other errors if any
+    except:
+        print("Some error occured while reading the input file")
+        exit()
+
+    # Declaring List to store the count of each Octant ID
+    count = [0]*8
+
+    # Tagging the octants by help of the video provided in the assignment
+    for i in range(0, len(time)):
+        if (u_prime[i] >= 0 and v_prime[i] >= 0):
+            if w_prime[i] >= 0:
+                octant.append(1)
+                count[0] += 1
+            else:
+                octant.append(-1)
+                count[1] += 1
+        if (u_prime[i] < 0 and v_prime[i] >= 0):
+            if w_prime[i] >= 0:
+                octant.append(2)
+                count[2] += 1
+            else:
+                octant.append(-2)
+                count[3] += 1
+        if (u_prime[i] < 0 and v_prime[i] < 0):
+            if w_prime[i] >= 0:
+                octant.append(3)
+                count[4] += 1
+            else:
+                octant.append(-3)
+                count[5] += 1
+        if (u_prime[i] >= 0 and v_prime[i] < 0):
+            if w_prime[i] >= 0:
+                octant.append(4)
+                count[6] += 1
+            else:
+                octant.append(-4)
+                count[7] += 1
+
+    # Defining Ranges with help of mod
+    range1 = []
+
+    # Also defining a list called label to store some label texts for column L in the excel sheet
+    label = [" "]
+    range_count = 0
+
+    # With each step in the loop, if the condition is satisfied we increase the range count by 1 and also append a blank space in label for convenience in later steps
+    for x in range(0, len(time), mod):
+        if x == 0:
+            range1.append(".0000-{}".format(mod-1))
+            range_count = range_count+1
+        elif x+mod > len(time):
+            range1.append("{}-{}".format(x, len(time)-1))
+            range_count = range_count+1
+        else:
+            range1.append("{}-{}".format(x, x+mod-1))
+            range_count = range_count+1
+
+    # Extending range1 and label list
+    range1.extend(("Overall Transition Count", " ", "Octant #",
+                   "+1", "-1", "+2", "-2", "+3", "-3", "+4", "-4"))
+    label.extend(("", "", "", "From", "", "", "", "", "", "", ""))
+
+    # Declaring Lists to store the count of each octant ID in the given mod
+    mod_c1 = []
+    mod_cm1 = []
+    mod_c2 = []
+    mod_cm2 = []
+    mod_c3 = []
+    mod_cm3 = []
+    mod_c4 = []
+    mod_cm4 = []
+    # Here cmi refers to -ith octant
+
+    # Overall Transition Count
+    overall_transition_list = []
+
+    # Counting the octant values in each mod using a loop method for a particular range in the octant list
+    z = 0
+    y = mod
+    while y-mod < len(time):
+        mod_c1.append(octant[z:y].count(1))
+        mod_cm1.append(octant[z:y].count(-1))
+        mod_c2.append(octant[z:y].count(2))
+        mod_cm2.append(octant[z:y].count(-2))
+        mod_c3.append(octant[z:y].count(3))
+        mod_cm3.append(octant[z:y].count(-3))
+        mod_c4.append(octant[z:y].count(4))
+        mod_cm4.append(octant[z:y].count(-4))
+        z = y
+        y = y+mod
+
+    # In this step we also extend the lists to print the column headers for overall transition count
+    mod_c1.extend((" ", "To", "+1"))
+    mod_cm1.extend((" ", " ", "-1"))
+    mod_c2.extend((" ", " ", "+2"))
+    mod_cm2.extend((" ", " ",  "-2"))
+    mod_c3.extend((" ", " ", "+3"))
+    mod_cm3.extend((" ", " ",  "-3"))
+    mod_c4.extend((" ", " ", "+4"))
+    mod_cm4.extend((" ", " ",  "-4"))
+
+    # A simple 2D loop to check the transitions. I am using two loops and then adding the count to overall_transition list
+    for j in range(8):
+        octant_list = [1, -1, 2, -2, 3, -3, 4, -4]
+        overall_transition = [0]*8
+        for i in range(len(time)-1):
+            if octant[i+1] == octant_list[j]:
+                if octant[i] == 1:
+                    overall_transition[0] += 1
+                elif octant[i] == -1:
+                    overall_transition[1] += 1
+                elif octant[i] == 2:
+                    overall_transition[2] += 1
+                elif octant[i] == -2:
+                    overall_transition[3] += 1
+                elif octant[i] == 3:
+                    overall_transition[4] += 1
+                elif octant[i] == -3:
+                    overall_transition[5] += 1
+                elif octant[i] == 4:
+                    overall_transition[6] += 1
+                elif octant[i] == -4:
+                    overall_transition[7] += 1
+
+        # overall_transition_list is the list to store the 8 lists we obtained in the above loop
+        overall_transition_list.append(overall_transition)
+
+    # To print the data to the excel file, we append the overall_transition_list's values to the respective columns using a loop
+    for i in range(8):
+        mod_c1.append(overall_transition_list[0][i])
+        mod_cm1.append(overall_transition_list[1][i])
+        mod_c2.append(overall_transition_list[2][i])
+        mod_cm2.append(overall_transition_list[3][i])
+        mod_c3.append(overall_transition_list[4][i])
+        mod_cm3.append(overall_transition_list[5][i])
+        mod_c4.append(overall_transition_list[6][i])
+        mod_cm4.append(overall_transition_list[7][i])
+    overall_transition_list.clear()
+
+    # Mod Transition Counts
+    # We start with a loop iterating the upper round off of 30000/mod
+    z = 0
+    y = mod
+    for x in range(math.ceil(len(time)/mod)):
+        # A special if condition, to handle the last case
+        if y >= len(time):
+            y = len(octant)-1
+
+        # Extending the lists to label the lists for each iteration
+        range1.extend(("", "", " ", "Mod Transition Count",
+                       range1[x], "Octant #", "+1", "-1", "+2", "-2", "+3", "-3", "+4", "-4"))
+        label.extend(("", "", "", "", "", "", "From",
+                      "", "", "", "", "", "", ""))
+        mod_c1.extend(("", " ", " ", " ", "To", "+1"))
+        mod_cm1.extend(("", " ", " ", " ", " ", "-1"))
+        mod_c2.extend(("", " ", " ", " ", " ", "+2"))
+        mod_cm2.extend(("", " ", " ", " ", " ", "-2"))
+        mod_c3.extend(("", " ", " ", " ", " ", "+3"))
+        mod_cm3.extend(("", " ", " ", " ", " ", "-3"))
+        mod_c4.extend(("", " ", " ", " ", " ", "+4"))
+        mod_cm4.extend(("", " ", " ", " ", " ", "-4"))
+
+        # A simple 2D loop to check the transitions. I am using two loops and then adding the count to overall_transition list
+        for j in range(8):
+            octant_list = [1, -1, 2, -2, 3, -3, 4, -4]
+            overall_transition = [0]*8
+            for i in range(z, y):
+                if octant[i+1] == octant_list[j]:
+                    if octant[i] == 1:
+                        overall_transition[0] += 1
+                    elif octant[i] == -1:
+                        overall_transition[1] += 1
+                    elif octant[i] == 2:
+                        overall_transition[2] += 1
+                    elif octant[i] == -2:
+                        overall_transition[3] += 1
+                    elif octant[i] == 3:
+                        overall_transition[4] += 1
+                    elif octant[i] == -3:
+                        overall_transition[5] += 1
+                    elif octant[i] == 4:
+                        overall_transition[6] += 1
+                    elif octant[i] == -4:
+                        overall_transition[7] += 1
+
+        # overall_transition_list is the list to store the 8 lists we obtained in the above loop
+            overall_transition_list.append(overall_transition)
+
+    # To print the data to the excel file, we append the overall_transition_list's values to the respective columns using a loop
+        for i in range(8):
+            mod_c1.append(overall_transition_list[0][i])
+            mod_cm1.append(overall_transition_list[1][i])
+            mod_c2.append(overall_transition_list[2][i])
+            mod_cm2.append(overall_transition_list[3][i])
+            mod_c3.append(overall_transition_list[4][i])
+            mod_cm3.append(overall_transition_list[5][i])
+            mod_c4.append(overall_transition_list[6][i])
+            mod_cm4.append(overall_transition_list[7][i])
+        z = y
+        y = y+mod
+        # Clearing the lists for the iteration of next range
+        overall_transition_list.clear()
+
+    # Appending the remaining length of the lists with a blank string for convenience in later steps
+    for x in range(int(len(time)/mod)+2, len(time)):
+        mod_c1.append("")
+        mod_cm1.append("")
+        mod_c2.append("")
+        mod_cm2.append("")
+        mod_c3.append("")
+        mod_cm3.append("")
+        mod_c4.append("")
+        mod_cm4.append("")
+
+    # Appending the remaining length of the lists with a blank string for convenience in later steps
+    for x in range(range_count+1, len(time)):
+        range1.append("")
+        label.append("")
+
+    try:
+        # Declaring a list to store the output of lines/rows
+        output_row = []
+        for i in range(0, len(time)):
+            output_row.append([label[i+1], range1[i+4], mod_c1[i+4], mod_cm1[i+4], mod_c2[i+4],
+                               mod_cm2[i+4], mod_c3[i+4], mod_cm3[i+4], mod_c4[i+4], mod_cm4[i+4]])
+
+        # Writing the remaining values to the output file
+        # Here i is the range of columns and j is the range of rows. By combinations of characters we are storing the data to the corresponding cells.
+        for i in range(8, 18):
+            for j in range(1, len(time)):
+                cell = 'A'+chr(i+64)+str(j)
+                sheet[cell] = output_row[j-1][i-8]
+
+    except:
+        print("Something went wrong while writing to octant_output.csv")
+        exit()
 
 
 def octant_analysis(mod=5000):
     inputfile = "input/2.0.xlsx"
-    octant_range_names(inputfile, mod)
+    wb = Workbook()
+    sheet = wb.active
+    octant_range_names(sheet, inputfile, mod)
+    octant_transition_count(sheet, inputfile, mod)
+    # Saving the workbook.
+    wb.save("Output_File.xlsx")
+    wb.close()  # Closing the workbook.
 
 
 ver = python_version()
